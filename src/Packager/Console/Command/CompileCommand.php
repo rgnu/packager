49fb3653379d772ca93f3c29cf382b6da844e73d
Rodrigo Garcia <rodrigog@olx.com>
2013-03-11 17:40:47 -0300

    * Remove unused file Compiler.php * Create getStub for CompileCommand

--- src/Packager/Console/Command/CompileCommand.php
+++ src/Packager/Console/Command/CompileCommand.php
@@ -51,7 +51,6 @@ class CompileCommand extends Command
         $finder->files()
             ->ignoreVCS(true)
             ->name('*.php')
-            ->name('packager')
             ->in($root);
                
         $phar = new \Phar($filename, 0, $filename);
@@ -59,11 +58,36 @@ class CompileCommand extends Command
         $phar->startBuffering();
 
         $phar->buildFromIterator($finder, $root);
-        $phar->setStub($phar->createDefaultStub('bin/packager'));
+        
+        $this->addBin($phar, 'bin/packager', $root.'/bin/packager');
+        
+        $phar->setStub($this->getStub($filename));
 
         $phar->stopBuffering();
         $phar->compressFiles(\Phar::GZ);
 
         unset($phar);
     }
+    
+    private function addBin($phar, $name, $file)
+    {
+        $content = file_get_contents($file);
+        $content = preg_replace('{^#!/usr/bin/env php\s*}', '', $content);
+        $phar->addFromString($name, $content);
+    }    
+    
+    private function getStub($filename)
+    {
+        $format = <<<'EOF'
+#!/usr/bin/env php
+<?php
+
+Phar::mapPhar("%s");
+
+require 'phar://%s/bin/packager';
+
+__HALT_COMPILER();
+EOF;
+        return sprintf($format, $filename, $filename);
+    }    
 }
